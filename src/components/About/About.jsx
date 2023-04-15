d7143ff6c4d1825353418376e702a3a83a0a0667
Johan Fajardo <johan.fajardo@gmail.com>
2023-04-14 20:47:50 -0500

    Ultima Actualización

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,15 +9,10 @@ export default function About () {
             <h2>Ingeniero Electronico</h2> 
             <h2>FT 37-A</h2> 
             <img className={styles.img} src={johan} alt="Billy"/>
-            <div className={styles.div_button}>
-            <div className= {styles.button_git}>
-                <NavLink  to='https://github.com/JFFajardo'></NavLink>               
-            </div>
-            <div className= {styles.button_link}>
-                <NavLink  to='https://www.linkedin.com/in/johan-arciniegas-fajardo-81500511a'></NavLink>               
-            </div>
-            </div>
-          
+            <div className={styles.div_button}>                
+                <a className= {styles.button_git} href="https://github.com/JFFajardo"></a>
+                <a className= {styles.button_link}href='https://www.linkedin.com/in/johan-arciniegas-fajardo-81500511a'></a>
+            </div>         
             <h3>Cali - Colombia</h3>
         </div>
     )
