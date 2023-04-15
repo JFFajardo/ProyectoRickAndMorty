acfc1de1e52fcdd6cbc8a652e0b8e288d1595a43
Johan Fajardo <johan.fajardo@gmail.com>
2023-04-14 20:09:17 -0500

    Actualización Final Proyecto RyM

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import styles from "./About.module.css";
 import johan from '../../assets/img/johan.jpg'
+import { NavLink } from "react-router-dom";
 
 export default function About () {
     return(
@@ -8,6 +9,15 @@ export default function About () {
             <h2>Ingeniero Electronico</h2> 
             <h2>FT 37-A</h2> 
             <img className={styles.img} src={johan} alt="Billy"/>
+            <div className={styles.div_button}>
+            <div className= {styles.button_git}>
+                <NavLink  to='https://github.com/JFFajardo'></NavLink>               
+            </div>
+            <div className= {styles.button_link}>
+                <NavLink  to='https://www.linkedin.com/in/johan-arciniegas-fajardo-81500511a'></NavLink>               
+            </div>
+            </div>
+          
             <h3>Cali - Colombia</h3>
         </div>
     )
