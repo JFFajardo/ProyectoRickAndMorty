a1b6bbad14e59b9b3117b2d09b889004ea690673
Johan Fajardo <johan.fajardo@gmail.com>
2023-04-18 20:47:37 -0500

    cambios

--- Client/src/components/About/About.jsx
+++ Client/src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import styles from "./About.module.css";
 import johan from '../../assets/img/johan.jpg'
-import { NavLink } from "react-router-dom";
+
 
 export default function About () {
     return(
@@ -10,8 +10,8 @@ export default function About () {
             <h2>FT 37-A</h2> 
             <img className={styles.img} src={johan} alt="Billy"/>
             <div className={styles.div_button}>                
-                <a className= {styles.button_git} href="https://github.com/JFFajardo"></a>
-                <a className= {styles.button_link}href='https://www.linkedin.com/in/johan-arciniegas-fajardo-81500511a'></a>
+            <a className= {styles.button_git} href="https://github.com/JFFajardo"></a>
+            <a className= {styles.button_link}href='https://www.linkedin.com/in/johan-arciniegas-fajardo-81500511a'></a>
             </div>         
             <h3>Cali - Colombia</h3>
         </div>
--- Server/src/index.js
+++ Server/src/index.js
@@ -2,6 +2,8 @@ const http = require('http');
 
 http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+
+    
     res.writeHead(200, {'Content-type': 'text/plain'})
     res.end('Hola, creaste un server O.O')
    
