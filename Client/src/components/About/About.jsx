3f405880ba689b97c37c22d18298552b51ae295e
Johan Fajardo <johan.fajardo@gmail.com>
2023-04-28 22:27:50 -0500

    correción linea

--- Client/src/components/About/About.jsx
+++ Client/src/components/About/About.jsx
@@ -9,8 +9,7 @@ export default function About () {
             <h3>Ingeniero Electronico</h3> 
             <h3>FT 37-A</h3> 
             <img className={styles.img} src={johan} alt="Billy"/>
-            <div className={styles.div_button}>   
-            // eslint-disable-next-line             
+            <div className={styles.div_button}>                        
             <a className= {styles.button_git} href="https://github.com/JFFajardo"></a>
             <a className= {styles.button_link}href='https://www.linkedin.com/in/johan-arciniegas-fajardo-81500511a'></a>
             </div>         
